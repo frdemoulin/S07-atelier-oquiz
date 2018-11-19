01726c7dcdb512f15dfc5348e482a61890e3874c
freddmn08 <freddmn08@gmail.com>
2018-11-19 23:52:13 +0100

    prépa routes méth atelier J2

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,10 +6,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 
 //import de mon model pour effectuer des requetes
-use App\Quizzes;
-use App\Tags;
 use App\AppUsers;
-use App\QuizzesHasTags;
+// use App\Quizzes;
+// use App\QuizzesHasTags;
+// use App\Tags;
 
 /*
  Pour utiliser / recuperer l'objet Request, on doit obligatoirement importer cette classe Lumen
@@ -29,5 +29,76 @@ class UserController extends Controller
         //
     }
 
-    //
+    /**
+     * méthode en GET associée au endpoint /logout
+     * traite la déconnexion d'un utilisateur
+     *
+     * @param Request $request
+     * @param string $id
+     * @return json
+     */
+    // public function logout(Request $request)
+    // {
+    //     // on ouvre la session
+    //     session_start();
+    //     // on efface les données de session
+    //     session_unset();
+    //     // on détruit la session
+    //     session_destroy();
+    //     // on redirige vers le formulaire de connexion
+    //     header('Location: connexion.php');
+    //     // on stoppe le script courant
+    //     exit();
+    // }
+
+    // /**
+    //  * méthode en POST associée au endpoint /signin
+    //  * traite le formulaire de connexion
+    //  *
+    //  * @param Request $request
+    //  * @return 
+    //  */
+    // public function signinPost(Request $request)
+    // {
+    //     // on récupère les infos en POST du form de connexion
+    //     $email = $request->input('email');
+    //     $password = $request->input('password');
+
+    //     // contrôle d'intégrité des données
+
+    //     // on redirige vers la page du compte utilisateur
+    //     return redirect()->route('account');
+    // }
+
+    // /**
+    //  * méthode en POST associée au endpoint /signup
+    //  * traite le formulaire d'inscription
+    //  *
+    //  * @param Request $request
+    //  * @return 
+    //  */
+    // public function signupPost(Request $request)
+    // {
+    //     // on récupère les infos en POST du form d'inscription
+    //     $email = $request->input('email');
+    //     $password = $request->input('password');
+    //     $firstname = $request->input('firstname');
+    //     $lastname = $request->input('lastname');
+
+    //     // contrôle d'intégrité des données
+
+    //     $user = new AppUsers();
+
+    //     // on stocke les infos en bdd
+    //     // $user->email = request('email');
+    //     $user->email = $email;
+    //     $user->password = $password;
+    //     $user->firstname = $firstname;
+    //     $user->lastname = $lastname;
+    //     $user->save();
+
+    //     // on redirige vers la page du compte utilisateur
+    //     return redirect()->route('account');
+    // }
+
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,30 +11,113 @@
 |
 */
 
-// route en get associée au endpoint /
+/*
+************************
+* Routes MainController
+************************
+*/
+
+// route en GET associée au endpoint /
 // liste tous les quiz
 $router->get('/', [
     'as' => 'home', // clef = as => valeur = url
     'uses' => 'MainController@displayHome' // le code qui doit être exécuté et qui se situe dans MainController@laMethode à appeler. C'est Lumen qui se charge de découper MainController@laMethode sur le arobase (# avec AltoRouter)
 ]);
 
-// route en get associée au endpoint /quiz/[id]
+/*
+************************
+* Routes QuizController
+************************
+*/
+
+// route en GET associée au endpoint /quiz/[id]
 // affichage du quiz d'id donné
 $router->get('/quiz/{id}', [
     'as' => 'quiz',
     'uses' => 'QuizController@quiz'
 ]);
 
-// route en get associée au endpoint /tags
+// route en POST associée au endpoint /quiz/[id]
+// affichage du quiz d'id donné
+$router->post('/quiz/{id}', [
+    'as' => 'quiz_post',
+    'uses' => 'QuizController@quizPost'
+]);
+
+// route en GET associée au endpoint /tags
 // affichage de tous les tags (sujets de quiz)
 $router->get('/tags', [
     'as' => 'tags',
     'uses' => 'QuizController@tags'
 ]);
 
-// route en get associée au endpoint /tags/[id]/quiz
+// route en GET associée au endpoint /tags/[id]/quiz
 // liste tous les quiz associés au tag d'id donné
 $router->get('/tags/{id}/quiz', [
     'as' => 'list_by_tag',
     'uses' => 'QuizController@listByTag'
+]);
+
+/*
+************************
+* Routes UserController
+************************
+*/
+
+/**
+ * SIGNUP
+ */
+
+// route en GET associée au endpoint /signup
+// gère le formulaire d'inscription
+$router->get('/signup', [
+    'as' => 'signup',
+    'uses' => 'UserController@signup'
+]);
+
+// route en POST associée au endpoint /signup
+// traite le formulaire d'inscription
+$router->post('/signup', [
+    'as' => 'signup_post',
+    'uses' => 'UserController@signupPost'
+]);
+
+/**
+ * SIGNIN
+ */
+
+// route en GET associée au endpoint /signin
+// gère le formulaire de connexion
+$router->get('/signin', [
+    'as' => 'signin',
+    'uses' => 'UserController@signin'
+]);
+
+// route en POST associée au endpoint /signin
+// traite le formulaire de connexion
+$router->post('/signin', [
+    'as' => 'signin_post',
+    'uses' => 'UserController@signinPost'
+]);
+
+/**
+ * ACCOUNT
+ */
+
+// route en GET associée au endpoint /account
+// page profil de l’utilisateur connecté
+$router->get('/account', [
+    'as' => 'account',
+    'uses' => 'UserController@profile'
+]);
+
+/**
+ * LOGOUT
+ */
+
+// route en GET associée au endpoint /logout
+// page traitant la déconnexion de l’utilisateur
+$router->get('/logout', [
+    'as' => 'logout',
+    'uses' => 'UserController@logout'
 ]);
